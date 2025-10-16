7d994bffafa2eca1272222eb49e6c392a04137ac
Brodie <brodiecarter067@gmail.com>
2025-10-16 08:57:55 -0400

    ] ohhhhhh#

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -2,15 +2,13 @@
 ##### Template for Chapter 5.14, Exercises 1 - 4 ######
 
 
-print("********** Ch 5 Exercise 1 **********")
-
 def time_since_epoch():
     import time
     T= time.time()
-    #print(T)
+    print(T)
     days =int (T // 86400)
     print(days)
-time_since_epoch()
+#time_since_epoch()
 
 
 
@@ -26,11 +24,27 @@ time_since_epoch()
 
 ##print("********** Ch 5 Exercise 3 **********")
 
-# Do your work for Exercise 3 here.
+def is_triangle(a, b, c):
+    print('is triangle()', a, b, c)
+    if a>=b + c:
+        print('Not a triangle')
+    elif b>= a + c:
+        print('Not a triangle')
+    elif c>= a + b:
+        print('Not a triangle')
+    else:
+        print('Is a triangle')
+    
+a = float(input('how long is side a?'))
+print('a is', a, type(a))
 
-#print("Ch 5 Exercise 3: Not implemented") # Delete this line when you write your code!
+b = float(input('how long is side b?'))
+print('b is', b, type(b))
 
+c = float(input('how long is side c?'))
+print('c is', c, type(c))
 
+is_triangle(a, b, c)
 
 #print("********** Ch 5 Exercise 4 **********")
 
--- chapters/05_2_homework/hw1_1.py
+++ chapters/05_2_homework/hw1_1.py
@@ -4,7 +4,17 @@
 
 print("********** Homework 1 Exercise 1 **********")
 
-# Do your work for Exercise 1 here
+def print_tic_tac_toe():
+    horizontal_line = '--------'
+    vertical_line = '  |  | '
+    print(vertical_line)
+    print(horizontal_line)
+    print(vertical_line)
+    print(horizontal_line)
+    print(vertical_line)
+
+print_tic_tac_toe()
+
 
 print("Homework 1 Exercise 1: Not implemented") # Delete this line when you write your code!
 
@@ -20,6 +30,4 @@ print("Homework 1 Exercise 2: Not implemented") # Delete this line when you writ
 
 print("********** Homework 1 Exercise 3 **********")
 
-# Do your work for Excercise 3 here
 
-print("Homework 1 Exercise 3: Not implemented") # Delete this line when you write your code!
