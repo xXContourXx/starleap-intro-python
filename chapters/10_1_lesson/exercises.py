192136fb3a90f9c618230e3e5d3dd1642ec54c19
Emily Neel <emily@pbase.com>
2025-10-18 11:50:44 -0400

    Ch 10 exercises template added

---
+++ chapters/10_1_lesson/exercises.py
@@ -0,0 +1,67 @@
+
+##### Template for Chapter 10 Exercises 1 - 7, 9 - 12 ######
+
+print("********** Ch 10 Exercise 1 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 2 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 3 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 4 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 5 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 6 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 7 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 9 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 10 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 11 **********")
+
+
+
+
+
+print("********** Ch 10 Exercise 12 **********")
+
+
+
