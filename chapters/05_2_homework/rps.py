7830225ca749c3edd0c27108759b147c6de42ab1
Brodie <brodiecarter067@gmail.com>
2025-10-23 09:55:05 -0400

    Week 10 work

--- chapters/05_2_homework/rps.py
+++ chapters/05_2_homework/rps.py
@@ -1,9 +1,24 @@
 def rock_paper_scissors():
-    if (player1 == 'rock' and player2 == 'scissors'):
-        print('Player1 wins')
-    if(player1 == 'paper' and player2 == 'rock'):
+
+    player1=input('Choose rock, paper, or scissors:')
+
+    player2=input('Choose rock, paper, or scissors:')
+    
+    if (player1== 'rock' and player2== 'scissors'):
         print('player1 wins')
-    if(player1 == 'scissors' and player2 == 'paper'):
-        print('player1 wins')
+    elif (player1== 'paper' and player2== 'rock'):
+         print('player1 wins')
+    elif (player1== 'scissors' and player2== 'paper'):
+         print('player1 wins')
+    elif (player1== player2):
+         print('tie')
+        
     else:
         print('player2 wins')
+
+rock_paper_scissors()
+
+
+
+
+    
